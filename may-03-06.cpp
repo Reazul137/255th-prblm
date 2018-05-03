96425db91b26fb927043f79364ff6e046719ae2f
Reazul Islam <reazulislam137@gmail.com>
2018-05-03 19:54:46 +0600

    first commit

---
+++ may-03-06.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string name;
+
+    cout << "Enter your fullname: "<<endl;
+    getline(cin, name);
+    cout << name << endl;
+
+
+    return 0;
+}
